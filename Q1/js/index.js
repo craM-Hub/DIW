4fe2d2bc0515435147f19f279f9e3beb1be14709
Marc Llop <anarka84@gmail.com>
2021-09-30 10:05:18 +0200

    Q1 subida archivos

---
+++ Q1/js/index.js
@@ -0,0 +1,8 @@
+function showHide(nombre) {
+  var x = document.getElementById(nombre);
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
